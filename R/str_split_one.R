2658e95a264859fe70e8f0a17e916ff9a1696c41
Stanley <nmorstanlee@gmail.com>
2024-02-21 12:43:46 +0100

    Fix error in arg name

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,9 +1,8 @@
 
 #' Split a string
 #'
-#' @param x A character vector with, at most, one element.
+#' @param string A character vector with, at most, one element.
 #' @inheritParams stringr::str_split
-#' @param split What to split on.
 #'
 #' @return A character vector.
 #' @export
